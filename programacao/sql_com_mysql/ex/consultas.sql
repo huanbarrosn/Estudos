8331fd6e2c08541aafe94ddfc733017b4318be6f
Huan Barros <audax.py@gmail.com>
2023-03-08 23:35:53 -0400

    update estudos

--- programacao/sql_com_mysql/ex/consultas.sql
+++ programacao/sql_com_mysql/ex/consultas.sql
@@ -11,3 +11,17 @@ ON t1.product_id = t2.product_id
 
 GROUP BY t2.product_category_name
 ORDER BY SUM(t1.price) DESC;
+
+
+
+SELECT DISTINCT seller_id, product_category_name
+FROM tb_order_items AS t1
+
+INNER JOIN tb_products AS t2
+ON t1.product_id = t2.product_id
+
+ORDER BY seller_id;
+
+
+
+
